45f713a7709fb8fca2b6957a715f1a1c512f9336
Daniel Crovo <danielcrovop@gmail.com>
2023-09-26 12:33:50 -0500

    Reformated code added Cmake

---
+++ build/CMakeFiles/Neutrino-Pulse-Generator.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/danielcrovo/Documents/01.Study/01.MSc/01.Thesis/04.TG2/Semester/Neutrino-Pulse-Generator/src/xorrshiro128.c" "CMakeFiles/Neutrino-Pulse-Generator.dir/src/xorrshiro128.c.o" "gcc" "CMakeFiles/Neutrino-Pulse-Generator.dir/src/xorrshiro128.c.o.d"
+  "/home/danielcrovo/Documents/01.Study/01.MSc/01.Thesis/04.TG2/Semester/Neutrino-Pulse-Generator/src/generate_exp_decay_array.cc" "CMakeFiles/Neutrino-Pulse-Generator.dir/src/generate_exp_decay_array.cc.o" "gcc" "CMakeFiles/Neutrino-Pulse-Generator.dir/src/generate_exp_decay_array.cc.o.d"
+  "/home/danielcrovo/Documents/01.Study/01.MSc/01.Thesis/04.TG2/Semester/Neutrino-Pulse-Generator/src/main.cc" "CMakeFiles/Neutrino-Pulse-Generator.dir/src/main.cc.o" "gcc" "CMakeFiles/Neutrino-Pulse-Generator.dir/src/main.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Neutrino-Pulse-Generator.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Neutrino-Pulse-Generator.dir/src/generate_exp_decay_array.cc.o"
+  "CMakeFiles/Neutrino-Pulse-Generator.dir/src/generate_exp_decay_array.cc.o.d"
+  "CMakeFiles/Neutrino-Pulse-Generator.dir/src/main.cc.o"
+  "CMakeFiles/Neutrino-Pulse-Generator.dir/src/main.cc.o.d"
+  "CMakeFiles/Neutrino-Pulse-Generator.dir/src/xorrshiro128.c.o"
+  "CMakeFiles/Neutrino-Pulse-Generator.dir/src/xorrshiro128.c.o.d"
+  "Neutrino-Pulse-Generator"
+  "Neutrino-Pulse-Generator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Neutrino-Pulse-Generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
